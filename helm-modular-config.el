49aaa01a5d83d4f097507b457ca372c305c55652
CommanderAsdasd <e1ectricwiz4rd@gmail.com>
2022-04-29 16:16:06 +0200

    Update helm-modular-config.el
    Extend description to emacs packag standard

--- helm-modular-config.el
+++ helm-modular-config.el
@@ -1,5 +1,33 @@
-;; helm-modular-config.el --- helm interface for modular-config
-;; Commander asdasd
+;; helm-modular-config.el --- helm interface for modular-config 
+;; Copyright (C) 2022  Commander asdasd
+
+;; Author: Commander Asdasd <e1ectricwiz4rd@gmail.com>
+;; Keywords: convenience, helm
+;; Created: 29.04.2022
+;; Version: 0.1
+;; URL: "https://github.com/CommanderAsdasd/emacs-helm-modular-config"
+;; Package-Requires: ((emacs "25.1") (helm "3.0") (modular-config "0.5"))
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Helm interface for modular-config.el package.  Contributions welcome.
+
+;;; Code:
 
 (defun helm-modular-config/module-list-candidates ()
   "return list of strings - module load candidates"
